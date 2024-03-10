c399b56d85abefa85c5e3970b996ba684f902215
Ger2k <musikadicto@gmail.com>
2024-03-10 21:32:00 +0100

    probando API posters 5

--- front/js/funciones.js
+++ front/js/funciones.js
@@ -5,13 +5,7 @@ const textoTitulo = document.querySelector("form input:first-child");
 //CARGA INICIAL
 ajax("/lista").then(peliculas => {
 	peliculas.forEach(({_id,titulo,terminada}) => {
-		getPosterURL(){
-			fetch(`https://omdbapi.com/?apikey=b45635c9&s=${this.textoTitulo}`)
-			.then(res => res.json())
-			.then(data => () => {
-				data.Poster = posterURL;
-			})
-		}
+		
 		new Pelicula(_id,titulo,terminada,contenedor,posterURL);
 	})
 });
