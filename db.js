f8c896a1a33c25127c48a8f14ed8208b641b098d
Ger2k <musikadicto@gmail.com>
2024-04-03 15:55:54 +0200

    keep alive db 3

--- db.js
+++ db.js
@@ -136,22 +136,18 @@ function editarTitulo(id,nuevoTitulo){
 module.exports = {nuevaPelicula,borrar,leerPeliculas,editarEstado,editarTitulo}
 
 // Función trivial que consulta una colección para mantener activa la base de datos
-function mantenerActivaDB() {
-    conectar().then(conexion => {
+async function mantenerActivaDB() {
+    try {
+        const conexion = await conectar().connect(); // Asegúrate de llamar a .connect()
         const coleccion = conexion.db("peliculas").collection("peliculas");
-        // Realizar una consulta trivial, como contar los documentos
-        coleccion.countDocuments()
-            .then(count => {
-                console.log(`Hay ${count} películas. DB activa.`);
-                conexion.close();
-            })
-            .catch(err => {
-                console.error("Error manteniendo la DB activa:", err);
-                conexion.close();
-            });
-    });
+        const count = await coleccion.countDocuments();
+        console.log(`Hay ${count} películas. DB activa.`);
+        await conexion.close();
+    } catch (err) {
+        console.error("Error manteniendo la DB activa:", err);
+    }
 }
 
 // Establecer un intervalo para mantener la base de datos activa
 // 300000 ms equivalen a 5 minutos
-setInterval(mantenerActivaDB, 300000);
+setInterval(mantenerActivaDB, 10000);
